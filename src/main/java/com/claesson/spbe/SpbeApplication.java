73105b185a4e82125f52b61ecf2af5529e796aeb
linnclaesson <linn.denise.claesson@gmail.com>
2025-04-03 09:20:49 +0200

    Initial commit

---
+++ src/main/java/com/claesson/spbe/SpbeApplication.java
@@ -0,0 +1,13 @@
+package com.claesson.spbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claesson/spbe/SpbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claesson.spbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
